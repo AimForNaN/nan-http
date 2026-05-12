ed3698919d4866f5a1cc384ca1bbe2953acde88e
AimForNaN <aimfornan@localhost>
2026-05-12 16:45:13 -0400

    Update tests.

--- tests/ServerRequestFactoryTest.php
+++ tests/ServerRequestFactoryTest.php
@@ -5,21 +5,24 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use NaN\Http\ServerRequestFactory;
 use function NaN\Tests\expect;
 
-$nan_server_request = new ServerRequestFactory()->createServerRequest('', '', $_SERVER);
+$nan_server_request = new ServerRequestFactory()->createServerRequest('', '');
 
 try {
 	expect($nan_server_request->getCookieParams())->toBe($_COOKIE)
 		->and($nan_server_request->getQueryParams())->toBe($_GET)
 		->and($nan_server_request->getServerParams())->toBe($_SERVER)
-		->and($nan_server_request->getParsedBody())->toBe(
+		->and($nan_server_request->getMethod())->toBe($_SERVER['REQUEST_METHOD'])
+		->and((string)$nan_server_request->getUri())->toBe($_SERVER['REQUEST_URI'])
+	;
+
+	if ($nan_server_request->getMethod() === 'POST') {
+		expect($nan_server_request->getParsedBody())->toBe(
 			\json_decode(
 				\file_get_contents('php://input'),
 				true
 			)
-		)
-		->and($nan_server_request->getMethod())->toBe($_SERVER['REQUEST_METHOD'])
-		->and((string)$nan_server_request->getUri())->toBe($_SERVER['REQUEST_URI'])
-	;
+		);
+	}
 } catch (\Throwable $e) {
 	\http_response_code(500);
 	print_r($e->getTrace());
--- tests/client.php
+++ tests/client.php
@@ -3,12 +3,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Exception\RequestException;
 
 $client = new GuzzleHttp\Client([
 	'base_uri' => 'http://localhost:9001/',
 ]);
 
 try {
+	$rsp = $client->get('/ServerRequestFactoryTest.php?test=test');
 	$rsp = $client->post('/ServerRequestFactoryTest.php', [
 		'body' => json_encode([
 			'test' => 'test',
@@ -20,7 +22,7 @@ try {
 			'Content-Type' => 'application/json',
 		],
 	]);
-} catch (\GuzzleHttp\Exception\RequestException $e) {
+} catch (RequestException $e) {
 	$rsp = $e->getResponse();
 	echo $rsp->getBody();
 }
