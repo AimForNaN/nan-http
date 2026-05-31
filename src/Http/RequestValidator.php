994c824cda50237a1028a120354d965eebee1682
AimForNaN <aimfornan@localhost>
2026-05-30 20:40:09 -0400

    Refactoring.

--- src/Http/RequestValidator.php
+++ src/Http/RequestValidator.php
@@ -2,15 +2,23 @@
 
 namespace NaN\Http;
 
-use Nette\Schema\{Processor, Schema};
+use Nette\Schema\{
+	Elements\Structure,
+	Processor,
+};
 use Psr\Http\Message\ServerRequestInterface as PsrServerRequestInterface;
 
-class RequestValidator {
-	public function validate(PsrServerRequestInterface $request, Schema $schema): mixed {
+readonly class RequestValidator {
+	public function __construct(
+		public Structure $schema,
+	) {
+	}
+
+	public function validate(PsrServerRequestInterface $request): mixed {
 		$method = $request->getMethod();
 		$data = $method === 'GET' ? $request->getQueryParams() : $request->getParsedBody();
 		$processor = new Processor();
 
-		return $processor->process($schema, $data);
+		return $processor->process($this->schema, $data);
 	}
 }
