c93f9bb3f917c80fb1988e76ee012d99373190d8
AimForNaN <aimfornan@localhost>
2026-08-13 23:47:40 -0400

    ClosureRequestHandler now supports service containers.

--- src/Http/Request/Handlers/ClosureRequestHandler.php
+++ src/Http/Request/Handlers/ClosureRequestHandler.php
@@ -2,21 +2,52 @@
 
 namespace NaN\Http\Request\Handlers;
 
-use Psr\Http\Message\{
-	ResponseInterface as PsrResponseInterface,
-	ServerRequestInterface as PsrServerRequestInterface,
+use NaN\DI\{
+	Arguments,
+	DelegatesContainer,
 };
-use Psr\Http\Server\RequestHandlerInterface as PsrRequestHandlerInterface;
+use NaN\Http\ServerRequest;
+use Psr\Container\{
+	ContainerExceptionInterface as PsrContainerExceptionInterface,
+	ContainerInterface as PsrContainerInterface,
+	NotFoundExceptionInterface as PsrNotFoundExceptionInterface,
+};
+use Psr\Http\{
+	Message\ResponseInterface as PsrResponseInterface,
+	Message\ServerRequestInterface as PsrServerRequestInterface,
+	Server\RequestHandlerInterface as PsrRequestHandlerInterface,
+};
 
-readonly class ClosureRequestHandler implements PsrRequestHandlerInterface{
+readonly class ClosureRequestHandler implements PsrRequestHandlerInterface {
 	public function __construct(
 		private \Closure $__callback,
 	) {
 	}
 
-	public function handle(PsrServerRequestInterface $request): PsrResponseInterface {
+	/**
+	 * @throws PsrContainerExceptionInterface
+	 * @throws \ReflectionException
+	 * @throws PsrNotFoundExceptionInterface
+	 */
+	public function handle(
+		PsrServerRequestInterface $request,
+	): PsrResponseInterface {
 		$callback = $this->__callback;
+		$container = new DelegatesContainer([
+			PsrServerRequestInterface::class => $request,
+		]);
+		$services = ServerRequest::getServiceFromRequest(
+			PsrContainerInterface::class,
+			$request,
+		);
 
-		return $callback($request);
+		if ($services) {
+			$container = $container->withDelegates($services);
+		}
+
+		$args = Arguments::fromCallable($callback);
+		$resolved = $args->resolve($request->getQueryParams(), $container);
+
+		return $callback(...$resolved);
 	}
 }
